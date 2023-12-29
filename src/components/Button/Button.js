e1c0fdf10753481ac1aa079ca858063341036e3a
Delcio Polanco <delciopolanco@gmail.com>
2023-12-28 21:14:19 -0400

    creating button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,33 @@
+import classNames from "classnames";
+
+export const Button = ({
+  label,
+  type,
+  disabled,
+  onClick,
+  outline,
+  fullWidth,
+  leftIcon,
+  rigthIcon,
+}) => {
+  const clasess = classNames({
+    "w-auto p-5 content-center h-10 rounded-full font-semibold hover:cursor-pointer grid grid-cols-[1fr_auto]": true,
+    "text-white bg-primary hover:bg-primaryLight": !outline,
+    "text-gray-900 bg-gray-200 hover:bg-gray-100": outline,
+    "w-full": fullWidth,
+  });
+
+  return (
+    <button
+      aria-label={label}
+      disabled={disabled}
+      onClick={onClick}
+      type={type}
+      className={clasess}
+    >
+      {leftIcon ?? leftIcon}
+      <span>{label}</span>
+      {rigthIcon ?? rigthIcon}
+    </button>
+  );
+};
